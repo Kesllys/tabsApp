f0a5c86664e77bc9b4029d5727c642207f885bf1
Ana Carolina Antonio de Sousa <carolisousa102016@gmail.com>
2022-09-16 22:20:33 -0300

    tabsApp1
    16 do 09

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private toastController: ToastController) {}
+
+  async mostrarToast() {
+    const toast = await this.toastController.create({
+      message: 'Hello World!',
+      duration: 1500,
+      color: 'danger',
+      position: 'middle'
+    });
+
+    await toast.present();
+  }
 
 }
