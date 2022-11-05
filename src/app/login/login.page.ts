a6869d84df3eecfeec3d39bb720bf69b717007bc
Kelly Cardoso <kellycristinacardoso.713@gmail.com>
2022-11-04 22:06:16 -0300

    04.11

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+ formLogin = this.formBuilder.group({
+   email:['', Validators.compose([Validators.required, Validators.email])],
+   senha:['', Validators.compose([Validators.required, Validators.minLength(6)])]
+ });
+ 
+ mensagensErro = {
+email: [{tipo: 'required', aviso: 'Preencha o campo!'}, 
+{tipo: 'email', aviso: 'Tem que ser um e-mail!'}],
+senha: [{tipo: 'required', aviso: 'Digite a senha!'}, 
+{tipo: 'minLength', aviso: 'Digite mais!'}], 
+};
 
-  constructor() { }
+constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
